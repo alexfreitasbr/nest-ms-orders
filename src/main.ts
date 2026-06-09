df27baa53903e47e2fe331bcbca17db98aef7e23
alexfreitasbr <alexfreitasbr@hotmail.com>
2026-06-09 10:34:48 -0300

    base

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe, Logger } from '@nestjs/common';
+import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const logger = new Logger('OrdersMS-Main');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen();
+  logger.log(`Order Microservice is running on port ${envs.port}`);
+}
+bootstrap();
